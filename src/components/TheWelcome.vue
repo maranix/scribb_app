4087b40a66fef232db6b2b6f5e5433dc78211715
maranix <ramanverma4183@gmail.com>
2024-07-09 11:36:03 +0530

    create vue@latest

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-6xl font-bold">Welcome</h1>
+</template>
---
+++ src/components/__tests__/TheWelcome.spec.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+
+import { mount } from "@vue/test-utils";
+
+import TheWelcome from "../TheWelcome.vue";
+
+describe("TheWelcome", () => {
+	it("renders properly", () => {
+		const wrapper = mount(TheWelcome);
+		expect(wrapper.text()).toContain("Welcome");
+	});
+});
