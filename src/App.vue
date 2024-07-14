5c3e8b12da1dd97c6953a4c0467ccf94bf60802a
maranix <ramanverma4183@gmail.com>
2024-07-14 12:25:48 +0530

    update Switch components & unit test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
-import Editor from "@/components/Editor.vue";
-import Switch from "@/components/Switch.vue";
 </script>
 
 <template>
-    <header class="flex w-full">
-        <Switch class="m-auto" label="Live Preview" :onToggle="() => { }" />
-    </header>
-
-    <main class="flex place-center h-screen mx-8">
-        <Editor class="h-5/6 m-auto" />
-    </main>
 </template>
--- src/components/Switch/__tests__/Switch.spec.ts
+++ src/components/Switch/__tests__/Switch.spec.ts
@@ -4,24 +4,10 @@ import { describe, expect, it, vi } from "vitest";
 import Switch from "../Switch.vue";
 
 describe("Switch", () => {
-	const label = "Test Label";
-	const onToggle = vi.fn();
-
-	it("renders the label correctly", () => {
-		const wrapper = mount(Switch, {
-			props: {
-				label,
-				onToggle,
-			},
-		});
-		expect(wrapper.text()).toContain("Test Label");
-	});
-
 	it("toggles the checkbox when clicked", async () => {
 		const onToggle = vi.fn();
 		const wrapper = mount(Switch, {
 			props: {
-				label,
 				onToggle,
 			},
 		});
@@ -43,22 +29,5 @@ describe("Switch", () => {
 
 		expect(checkbox.element.checked).toBe(false);
 		expect(onToggle).toHaveBeenCalledWith(false);
-	});
-
-	it("does not call onToggle if the checkbox state does not change", async () => {
-		const onToggle = vi.fn();
-		const wrapper = mount(Switch, {
-			props: {
-				label,
-				onToggle,
-			},
-		});
-
-		const checkbox = wrapper.find('input[type="checkbox"]');
-		await checkbox.setValue(false);
-
-		await wrapper.vm.$nextTick();
-
-		expect(onToggle).not.toHaveBeenCalled();
 	});
 });
